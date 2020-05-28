0a25dbd99d975324c88f16fadb19951eb225dd14
abdallahcoptan <abdallah.cisco@gmail.com>
2020-05-28 04:00:38 +0200

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-265058606f7102cfe9e7.js"
+    "url": "webpack-runtime-f46bfad04504421baa6d.js"
   },
   {
     "url": "framework-8e210829bcc33c32949c.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "023d425c88553194919335800049822d"
+    "revision": "e897856086ceb790194011d0ef1871e3"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-4efef167a65f33edc4e5.js"
